3183d6646db3d37ae535134853958679e526cedf
Fernanda Pestana <pestanafj@gmail.com>
2024-07-06 21:23:09 -0300

    Criação de Classes Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,37 @@
+import br.com.dio.desafio.dominio.*;
+
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Curso de POO em Java");
+        curso1.setCargaHoraria(40);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Curso curso2 = new Curso();
+        curso2.setTitulo("JavaScript");
+        curso2.setDescricao("Curso de JavaScript");
+        curso2.setCargaHoraria(30);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Java Como Programar");
+        mentoria1.setDescricao("Mentoria em Java com POO");
+        mentoria1.setData(LocalDate.now());
+
+        Mentoria mentoria2 = new Mentoria();
+        mentoria2.setTitulo("JavaScript Como Programar");
+        mentoria2.setDescricao("Mentoria em JavaScript com Node.js");
+        mentoria2.setData(LocalDate.now());
+
+        System.out.println(curso1.toString());
+        System.out.println(curso2.toString());
+        System.out.println(mentoria1.toString());
+        System.out.println(mentoria2.toString());
+
+
     }
 }
